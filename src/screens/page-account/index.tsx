7a476b56eca562dd1e83d92fe9065a531eb7083d
Victor Moisés <voliveira747@gmail.com>
2025-11-16 15:12:34 -0300

    feat: implementar tela de conta com navegação para login(incompleta)

--- src/screens/page-account/index.tsx
+++ src/screens/page-account/index.tsx
@@ -1,9 +1,31 @@
 import { View, Text } from "react-native";
+import{styles} from "./style";
+import { Button } from "@/src/components/ui/Button";
+import { GlobalText } from "@/src/components/ui/GlobalText";
+import { useAppNavigation } from "@/src/utils/navigation";
+
 
 export default function PageAccount() {
+  const navigation = useAppNavigation();
+
+  const handleGoToLogin = () => {
+    navigation.navigate('Login');
+  };
+
+
+
   return (
-    <View>
-      <Text>teste</Text>
+    <View style={styles.container}>
+      <View>  
+        <GlobalText variant={"bold"} style={styles.text_title}>Bem Vindo!</GlobalText>
+        <GlobalText variant={"regular"} style={styles.text_regular}>Junte-se a milhares de outras pessoas que fazem parte desse incrivel ecossistema</GlobalText>
+      </View>
+      <View>
+        <Button onPress={handleGoToLogin}>
+          Entra e Jogar
+        </Button>
+      </View>
+
     </View>
   );
 }
---
+++ src/screens/page-account/style.ts
@@ -0,0 +1,23 @@
+    import { colors } from "@/src/styles/theme";
+    import { StyleSheet } from 'react-native';
+
+    export const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            padding: 16,
+        },
+        text_title:{
+            paddingVertical: 18,
+            paddingHorizontal: 8,
+            fontSize: 45,
+            width: "100%",
+            textAlign: 'center',   
+        },
+        text_regular:{
+            fontSize: 18,
+            textAlign: 'center',
+            marginBottom: 20,
+            width: "100%",
+        },
+
+    });
