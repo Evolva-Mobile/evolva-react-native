fa6dcd34da9f62fe3e955c3475e096a237a267a2
Victor Moisés <voliveira747@gmail.com>
2025-12-12 14:55:16 -0300

    feat: adicionar modal de escolha de avatar na criação de jornada

--- src/screens/journey/create-journey/index.tsx
+++ src/screens/journey/create-journey/index.tsx
@@ -1,11 +1,16 @@
 
 import { HeaderBack } from "@/src/components/layout/headerBack";
 import { Button } from "@/src/components/ui/Button";
+import { GlobalText } from "@/src/components/ui/GlobalText";
 import { InputText } from "@/src/components/ui/InputText";
 import { InputToggle } from "@/src/components/ui/InputToggle";
+import { GlobalModal } from "@/src/components/ui/Modal";
+import { avatarList } from "@/src/screens/user/edit-account/avatarList";
+import { colors } from "@/src/styles/theme";
 import { useAppNavigation } from "@/src/utils/navigation";
 import { useState } from "react";
-import { Image, ImageSourcePropType, View } from "react-native";
+import { Image, ImageSourcePropType, TouchableOpacity, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { styles } from "./style";
 
 type userProps = {
@@ -61,7 +66,7 @@ export default function CreateJourney() {
                         <Image source={avatarSource} style={styles.avatarImg} />
                         <View style={styles.actionChoice}>
                             <Button color="neutral"  onPress={() => setVisible(true)}>Escolher</Button>
-                            {/* <ModalChoiceImg visible={visible} setVisible={setVisible} onConfirm={(img) => setSelectedAvatar(img)} /> */}
+                            <ModalChoiceImg visible={visible} setVisible={setVisible} onConfirm={(img) => setSelectedAvatar(img)} />
                         </View>
                     </View>
                     <View style={styles.firtsFilds}>
@@ -94,6 +99,66 @@ export default function CreateJourney() {
             </View>
         </View>
     );
+}
+
+function ModalChoiceImg({ visible, setVisible, onConfirm }: ModalChoiceImgProps) {
+    const [selected, setSelected] = useState<SelectedImgProp | any>({
+        width: 0,
+        uri: '',
+        height: 0
+    });
+
+    const onConfirmAvatar = () => {
+        onConfirm(selected);
+        setVisible(false);
+    }
+
+    return (
+        <GlobalModal
+            visible={visible}
+            onClose={() => setVisible(false)}
+        >
+            <View style={{
+                gap: 12,
+                alignItems: 'center'
+            }}>
+                <GlobalText variant="bold" style={{ fontSize: 20 }}>Escolha um avatar</GlobalText>
+                <GlobalText variant="medium" style={{ fontSize: 16, textAlign: 'center', color: colors.neutral80 }}>
+                    Esta foto sera exibida em seu perfil
+                </GlobalText>
+            </View>
+            <ScrollView style={{
+                height: 455,
+                marginVertical: 30,
+            }}>
+
+                <View style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    gap: 4,
+                    justifyContent: "center"
+                }}>
+                    {avatarList.map((img, index) => (
+                        <TouchableOpacity key={index} onPress={() => setSelected(img)}>
+                            <Image
+                                source={img}
+                                style={{
+                                    width: 90,
+                                    height: 90,
+                                    borderRadius: 50,
+                                    borderWidth: 3,
+                                    borderColor: selected === img ? colors.grenn100 : "transparent",
+                                    marginVertical: 10
+                                }}
+                            />
+                        </TouchableOpacity>
+                    ))}
+
+                </View>
+            </ScrollView>
+            <Button color="neutral" onPress={onConfirmAvatar}>Confirmar</Button>
+        </GlobalModal>
+    )
 }
 
 
